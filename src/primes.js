aefa656a7fe588deb14d0e8497ba4f86c301e2ce
mellowmorgan <morgan.waites@gmail.com>
2022-01-31 13:35:11 -0800

    refactor code
    Co-Authored-By: Kate Kiatsiri <keidsiri@gmail.com>

--- src/primes.js
+++ src/primes.js
@@ -1,14 +1,10 @@
 export default function primes(number, array = [...Array(number).keys()]){
-  return array.filter(function(element){
-    return isPrime(element);
-  });
   function isPrime(num, div = 3){
     if(num === 2) return true;
     if(num < 2 || num % 2 === 0)  return false;
     if(div * div > num) return true;
     if(num % div === 0) return false;
     return isPrime(num, div + 2);
-    
   }
+  return array.filter( e => isPrime(e));
 }
-
